02df4d2e64a0719b212b31c522c813f902a9c0d0
Kamilm55 <kamilmmmdov2905@gmail.com>
2024-04-23 18:51:06 +0400

    securing credentials

--- src/main/java/com/turingSecApp/turingSec/TuringSecApplication.java
+++ src/main/java/com/turingSecApp/turingSec/TuringSecApplication.java
@@ -2,10 +2,11 @@ package com.turingSecApp.turingSec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.turingSecApp.turingSec", "com.turingSecApp.turingSec.config"})
 public class TuringSecApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TuringSecApplication.class, args);
     }
