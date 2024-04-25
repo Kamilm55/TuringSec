6bcf92b3ee8ce63373a9ee031a6d777676338d90
Kamilm55 <kamilmmmdov2905@gmail.com>
2024-04-25 06:28:38 +0400

    local properties deleted

--- src/main/java/com/turingSecApp/turingSec/config/LocalPropertiesConfig.java
+++
@@ -1,9 +0,0 @@
-package com.turingSecApp.turingSec.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("file:C:\\Users\\User\\IdeaProjects\\Spring_Boot_Projects\\TuringSec\\src\\main\\resources\\local.properties")
-public class LocalPropertiesConfig {
-}
