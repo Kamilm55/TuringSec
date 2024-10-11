b7857af5d5c66ca52fd11b2d4a9d743deed96c80
Mamed0ff <rizvan.memmedov2004@gmail.com>
2024-10-11 21:04:04 +0400

    Server-sent event created and implemented. I have done some bug fixing and written LOGS for notification service and SSE

--- src/main/java/com/turingSecApp/turingSec/config/SecurityConfig.java
+++ src/main/java/com/turingSecApp/turingSec/config/SecurityConfig.java
@@ -81,7 +81,6 @@ public class SecurityConfig {
 
                     // Spring Security uses Ant-style path matching, where "/api/base-users/{baseUserId}" is treated as a pattern with a placeholder ({baseUserId}), often matching any single segment within that path (e.g., /api/base-users/123).
                     // The pattern "/api/base-users/current-user" is a specific literal match. It matches exactly and only /api/base-users/current-user
-                    request.requestMatchers(HttpMethod.GET,"/api/sse/notifications").anonymous();
                     request.requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll();
 
                     // CSRF controller
