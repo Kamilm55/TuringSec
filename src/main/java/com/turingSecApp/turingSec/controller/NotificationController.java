120bcb66d616da34a12e5bcba5cad3f275325541
Kanan Badirli <kanan.badirli@fpay.az>
2025-04-21 13:46:02 +0400

    Notification

--- src/main/java/com/turingSecApp/turingSec/controller/NotificationController.java
+++ src/main/java/com/turingSecApp/turingSec/controller/NotificationController.java
@@ -5,6 +5,7 @@ import com.turingSecApp.turingSec.response.message.NotificationDto;
 import com.turingSecApp.turingSec.service.interfaces.INotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/notification")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequiredArgsConstructor
 public class NotificationController {
 
